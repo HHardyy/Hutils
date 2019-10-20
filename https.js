f5380fdb941e890b5e0d96ea575476116542cb58
huangbingzhen <zhenbooks@163.com>
2019-10-20 11:51:49 +0800

    Update https.js

--- https.js
+++ https.js
@@ -1,31 +1,38 @@
 import axios from 'axios'
 import qs from 'qs'
 
-let baseUrl='http://api.hhardyy.com'
+let baseUrl = 'http://mock-api.com/jzA7P9nk.mock/api/testaxios'
 
-let apilist={
-  _postapi:'postapi',    //postapi address
-  _getapi:'getapi'       //getapi  address
+let _api = {}
+
+let http = {
+  post: '',
+  get: '',
+  baseUrl,
+  _api
 }
-let http={post:"",get:"",baseUrl:baseUrl,apilist:apilist}
-http.post=function(api,data){
-  let params=qs.stringify(data)
-  return new Promise((resolve,reject)=>{
-    axios.post(`${baseUrl}/${api}`,params).then(res=>{
+
+
+http.post = function (api, params) {
+  let _params = params ? qs.stringify(params) : ''
+  return new Promise((resolve, reject) => {
+    axios.post(`${baseUrl}${api ? '/' + api : ''}`, _params).then(res => {
       resolve(res)
-    }).catch(res=>{
+    }).catch(res => {
       reject(res)
     })
   })
 }
-http.get=function(api,data){
-  let params=qs.stringify(data)
-  return new Promise((resolve,reject)=>{
-    axios.get(`${baseUrl}/${api}`,params).then(res=>{
+
+http.get = function (api, params) {
+  let _params = params ? qs.stringify(params) : ''
+  return new Promise((resolve, reject) => {
+    axios.get(`${baseUrl}${api ? '/' + api : ''}`, _params).then(res => {
       resolve(res)
-    }).catch(res=>{
+    }).catch(res => {
       reject(res)
     })
   })
 }
+
 export default http
